2b54584f9cc776a9a9e792ce731b935d058b4464
Jonas Bondoc <jjbondoc07@gmail.com>
2026-02-23 21:29:30 +0800

    Update CORS origin to Netlify site

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ console.log("MONGO_URI =", process.env.MONGO_URI);
 
 const app = express();
 app.use(cors({
-  origin: process.env.CLIENT_URL
+  origin: 'https://your-netlify-site.netlify.app'
 }));
 app.use(express.json());
 
